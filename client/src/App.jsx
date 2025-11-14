a415e463df6b296855b5e27028d2b189863aad53
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-11-15 00:44:43 +0200

    user list added
    fetching data and visualize

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,14 @@ import Userlist from './components/UserList.jsx'
 import Pagination from './components/Pagination.jsx'
 import Footer from './components/Footer.jsx'
 import Search from './components/Search.jsx'
+import CreateUserModal from './components/createUserModal.jsx'
 
 function App() {
-
+    const [showCreateUser, setShowCreateUser] = useState(false);
+    
+    const createUserClickHandler =() => {
+        setShowCreateUser(true)
+    }
     return (
         <div>
             <Header />
@@ -17,11 +22,12 @@ function App() {
 
                     <Userlist />
 
+                    <button className="btn-add btn" onClick={createUserClickHandler}>Add new user</button>
+
                     <Pagination />
-
-                    <button className="btn-add btn">Add new user</button>
-
                 </section>
+                
+                {showCreateUser && <CreateUserModal />}
             </main>
 
             <Footer />
